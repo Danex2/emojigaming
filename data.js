a623946f0dc06dd319a12a903e7fdd16cc99faab
Danex2 <khadane.miller@gmail.com>
2019-10-02 15:09:45 -0400

    :sparkles: Added clue to games list, closes #2

--- data.js
+++ data.js
@@ -1,40 +1,50 @@
 const emojiItems = [
-    {
-        title: "Animal Crossing",
-        creator: ["Hasbro"],
-        emojiImgs: "🧙‍♂️🐍📓👩⚡️🐍",
-        genres: ["fantasy", "young-adult", "fiction", "magic"],
-        year: 1998,
-        type: "video-game",
-    },{
-        title: "Halo",
-        creator: ["J.K. Rowling"],
-        emojiImgs: "🧙‍♂️🐍📓👩⚡️🐍",
-        genres: ["fantasy", "young-adult", "fiction", "magic"],
-        year: 1998,
-        type: "video-game",
-    },{
-        title: "Oregon Trail",
-        creator: ["J.K. Rowling"],
-        emojiImgs: "🧙‍♂️🐍📓👩⚡️🐍",
-        genres: ["fantasy", "young-adult", "fiction", "magic"],
-        year: 1998,
-        type: "video-game",
-    },{
-        title: "Monopoly",
-        creator: ["J.K. Rowling"],
-        emojiImgs: "🧙‍♂️🐍📓👩⚡️🐍",
-        genres: ["fantasy", "young-adult", "fiction", "magic"],
-        year: 1998,
-        type: "video-game",
-    },{
-        title: "Battleships",
-        creator: ["J.K. Rowling"],
-        emojiImgs: "🧙‍♂️🐍📓👩⚡️🐍",
-        genres: ["fantasy", "young-adult", "fiction", "magic"],
-        year: 1998,
-        type: "video-game",
-    }
-]
-
-
+  {
+    title: 'Animal Crossing',
+    creator: ['Hasbro'],
+    emojiImgs: '🧙‍♂️🐍📓👩⚡️🐍',
+    genres: ['fantasy', 'young-adult', 'fiction', 'magic'],
+    year: 1998,
+    type: 'video-game'
+  },
+  {
+    title: 'Halo',
+    creator: ['J.K. Rowling'],
+    emojiImgs: '🧙‍♂️🐍📓👩⚡️🐍',
+    genres: ['fantasy', 'young-adult', 'fiction', 'magic'],
+    year: 1998,
+    type: 'video-game'
+  },
+  {
+    title: 'Oregon Trail',
+    creator: ['J.K. Rowling'],
+    emojiImgs: '🧙‍♂️🐍📓👩⚡️🐍',
+    genres: ['fantasy', 'young-adult', 'fiction', 'magic'],
+    year: 1998,
+    type: 'video-game'
+  },
+  {
+    title: 'Monopoly',
+    creator: ['J.K. Rowling'],
+    emojiImgs: '🧙‍♂️🐍📓👩⚡️🐍',
+    genres: ['fantasy', 'young-adult', 'fiction', 'magic'],
+    year: 1998,
+    type: 'video-game'
+  },
+  {
+    title: 'Battleships',
+    creator: ['J.K. Rowling'],
+    emojiImgs: '🧙‍♂️🐍📓👩⚡️🐍',
+    genres: ['fantasy', 'young-adult', 'fiction', 'magic'],
+    year: 1998,
+    type: 'video-game'
+  },
+  {
+    title: 'Clue',
+    creator: ['Anthony E. Pratt'],
+    emojiImgs: '🕵️‍🔪🎲🔧',
+    genres: ['murder-mystery', 'puzzle', 'strategy/tactics'],
+    year: 1949,
+    type: ['board-game']
+  }
+];
